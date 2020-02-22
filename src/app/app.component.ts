6afef16a485fe6a3eef460d2eaee34a9778c3394
davids-pro <54986025+davids-pro@users.noreply.github.com>
2020-02-22 18:06:19 +0100

    FINISHED !!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'ALGO';
+export class AppComponent implements OnInit {
+  nbCopy = 1;
+  priceCopy: number;
+
+  sex = 'homme';
+  age = 18;
+  taxeable: boolean;
+
+  ageConducteur = 18;
+  agePermis = 0;
+  nbAccident = 0;
+  anciennete = 0;
+  tarif: string;
+
+  ngOnInit() {
+    this.photocopies();
+    this.impots();
+    this.assurance();
+  }
+
+  photocopies() {
+    const initial = (copy: number) => {
+      return copy * 0.1;
+    };
+    const firstDiscount = (copy: number) => {
+      return copy * 0.09;
+    };
+    const secondDiscount = (copy: number) => {
+      return copy * 0.08;
+    };
+    if (this.nbCopy < 11) {
+      this.priceCopy = initial(this.nbCopy);
+    } else if (this.nbCopy >= 11 && this.nbCopy < 31) {
+      this.priceCopy = initial(10) + firstDiscount(this.nbCopy - 10);
+    } else {
+      this.priceCopy =
+        initial(10) + firstDiscount(20) + secondDiscount(this.nbCopy - 30);
+    }
+  }
+
+  impots() {
+    if (this.age < 18) {
+      this.taxeable = false;
+    } else if (this.age >= 18 && this.age <= 35) {
+      this.taxeable = true;
+    } else {
+      if (this.sex === 'femme') {
+        this.taxeable = false;
+      } else {
+        this.taxeable = true;
+      }
+    }
+  }
+
+  assurance() {
+    let points = 0;
+    if (this.ageConducteur >= 25) {
+      points++;
+    }
+    if (this.agePermis >= 2) {
+      points++;
+    }
+    switch (this.nbAccident) {
+      case 0:
+        points = points + 3;
+        break;
+      case 1:
+        points = points + 2;
+        break;
+      case 2:
+        points = points + 1;
+    }
+    if (points > 2) {
+      if (this.anciennete >= 1) {
+        points++;
+      }
+      if (points === 3) {
+        this.tarif = 'D';
+      } else if (points === 4) {
+        this.tarif = 'C';
+      } else if (points === 5) {
+        this.tarif = 'B';
+      } else if (points > 5) {
+        this.tarif = 'A';
+      }
+    } else {
+      this.tarif = null;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatToolbarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
